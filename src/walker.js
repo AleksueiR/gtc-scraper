2694c4560ca1871d28d82a1c306a2c92408273f1
aleksueir <ar.anteroom@live.ca>
2015-09-04 21:34:04 +0000

    feat(writer): improve writer and walker

--- src/walker.js
+++ src/walker.js
@@ -6,13 +6,14 @@ var p = require('path');
 var fuzzaldrin = require('fuzzaldrin');
 
 var Promise = require('promise');
-var readFile = Promise.denodeify(require('fs').readFile);
+var fs = require('fs-extra');
 
 var async = require('async');
 
 var sprintf = require("sprintf-js").sprintf;
 
 var writer = require('./writer');
+var ledger = require('./ledger');
 
 module.exports.examine = function() {
     console.log(process.argv); // use to parse arguments: https://www.npmjs.com/package/minimist
@@ -30,66 +31,114 @@ function askUser(dirname, matches) {
                     name: match.name, // + ' - ' + match.score,
                     value: i
                 };
-            }).concat({ name: '[ Skip ]', value: -1})
+            }).concat({
+                name: '[ Skip ]',
+                value: -1
+            })
         }, function(key) {
             fulfill(key);
         });
     });
 }
 
-readFile('ledger.json', 'utf8')
-    .then(function(data) {
-        var ledger = JSON.parse(data);
+function checkTvshownfo(dir) {
+    return walk.sync(dir, {
+        no_recurse: true
+    }).some(function(currentValue, index, array) {
+        return p.basename(currentValue) === 'tvshow.nfo';
+    });
+};
 
-        return ledger;
-    })
-    .then(function(ledger) {
+function processShow(dir) {
+    return new Promise(function(fulfill, reject) {
+
+        var dirname = p.basename(dir);
+        console.log(sprintf('"%s":', dirname));
+
+        if (checkTvshownfo(dir)) {
+            console.log(sprintf('   Existing metadata found; skipping...', dirname));
+            reject();
+            return;
+        }
+
+        var matches = fuzzaldrin.filter(ledger, dirname, {
+            key: 'name',
+            maxResults: 5
+        }).map(function(match) {
+            match.score = fuzzaldrin.score(match.name, dirname);
+            return match;
+        });
+
+        if (matches.length > 1) {
+            askUser(dirname, matches)
+                .then(function(key) {
+                    if (key.id === -1) {
+                        console.log(sprintf('   Skipping...'));
+                        reject();
+                    }
+                    else {
+                        fulfill({
+                            dirname: dirname,
+                            dir: dir,
+                            match: matches[key.id]
+                        });
+                    }
+                });
+        }
+        else if (matches.length === 0) {
+            console.log(sprintf('   Can\'t find a match; skipping...'));
+            reject();
+        }
+        else if (matches.length === 1 && matches[0].score < 0.08) {
+            console.log(sprintf('   Can\'t find a match; skipping... %s', matches[0].score));
+            reject();
+        }
+        else {
+            console.log(sprintf('   Matched to "%s" (%s)', matches[0].name, matches[0].score));
+
+            fulfill({
+                dirname: dirname,
+                dir: dir,
+                match: matches[0]
+            });
+        }
+    });
+}
+
+ledger.getLedger(true)
+    .then(function(ledgerData) {
+        ledger = ledgerData;
 
         var dirs = walk.sync(process.argv[2], {
             no_recurse: true
         });
 
-        //console.log(dirs);
+        console.log(sprintf('Found %s items:', dirs.length));
 
         async.eachSeries(dirs, function(dir, callback) {
             //console.log('found sync:', dir, p.basename(dir), fs.statSync(dir).isDirectory());
 
-            var dirname = p.basename(dir);
-            var matches = fuzzaldrin.filter(ledger, dirname, {
-                key: 'name',
-                maxResults: 5
-            }).map(function(match) {
-                match.score = fuzzaldrin.score(match.name, dirname);
-                return match;
-            });
-            
-            //console.log('a', matches); callback(); return;
-            
-            if (matches.length > 1) {
-                askUser(dirname, matches)
+            if (fs.statSync(dir).isDirectory()) {
+                processShow(dir)
+                    .then(function(record) {
+                        return writer.writeShowInfo(record)
+                    })
                     .then(function() {
+                        console.log('');
+                        callback();
+                    })
+                    .catch(function() {
+                        console.log('');
                         callback();
                     });
-            }
-            else if (matches.length === 0) {
-                console.log(sprintf('"%s": Can\'t find a match; skipping...', dirname));
-                callback();
-            }
-            else if (matches.length === 1 && matches[0].score < 0.08) {
-                console.log(sprintf('"%s": Can\'t find a match; skipping... %s', dirname, matches[0].score));
-                callback();
             }
             else {
-                console.log(sprintf('"%s": Matched to "%s" (%s)', dirname, matches[0].name, matches[0].score));
-                
-                writer
-                    .writeShowInfo(matches[0], dir)
-                    .then(function() {
-                        callback();
-                    });
+                callback();
             }
+
+
         }, function(err) {
-            console.log('done', err);
+            console.log('Scraping completed. Thank you.', err);
         });
 
     });
--- src/writer.js
+++ src/writer.js
@@ -1,22 +1,145 @@
 var fs = require('fs-extra');
 var p = require('path');
-var Download = require('download');
+var sprintf = require("sprintf-js").sprintf;
 
-// use  sudo apt-get install imagemagick to install image magic
-var gm = require('gm').subClass({
-    imageMagick: true
-});
+var walk = require('walkdir');
+var async = require('async');
+var Promise = require('promise');
+var find = require('array-find');
 
 var gtc = require('./gtc');
+var util = require('./util');
 
-module.exports.writeShowInfo = function(courseData, path) {
-  return gtc
-    .getCourseDetails(courseData)
-    .then(function(data) {
-        var show = data.lastReturn;
-        
-        fs.outputFileSync(path + '/' + show.getFileName(), show.toXML(), 'utf8');
-        
-        console.log(show.data.tvshow.title);
-    });
+module.exports.writeShowInfo = function(record) {
+
+    console.log(sprintf('   Removing old metadata...'));
+    walk.sync(record.dir)
+        .forEach(function(file) {
+            //console.log(file, p.extname(file));
+            if (p.extname(file) === '.nfo') {
+                fs.removeSync(file);
+            }
+        });
+
+    return gtc
+        .getCourseDetails(record.match)
+        .then(function(data) {
+            var show = data;
+
+            var episodeFiles = walk.sync(record.dir)
+                .filter(function(file) {
+                    return p.extname(file) === '.mkv';
+                });
+            var basefolder = record.dir + p.sep;
+
+            // output tvinfo.nfo file
+            console.log(sprintf('   Writing metadata...'));
+            fs.outputFileSync(basefolder + show.getFileName(), show.toXML(), 'utf8');
+
+            return processEpisodes(show, episodeFiles, basefolder)
+                .then(function() {
+                    console.log(sprintf('   Fetching images...'));
+                    
+                    //console.log('poster');        
+                    
+                    return getPoster(show, basefolder);
+                })
+                .then(function() {
+                    //console.log('fanart');
+                    return getFanart(show, basefolder);
+                })
+                .then(function() {
+                    //console.log('move');
+                    return moveShow(record.dir, p.dirname(record.dir) + p.sep + show.getShowTitle());
+                });
+            
+        })
+        .then(function() {
+            console.log('All done.');
+        });
 };
+
+function processEpisodes(show, episodeFiles, basefolder) {
+    return new Promise(function(fulfill, reject) {
+        async.eachSeries(show.episodes, function(episode, callback) {
+            
+            //console.log(episode);
+            
+            //show.episodes.forEach(function(episode) {
+            var episodeFile = find(episodeFiles, function(file) {
+                var reg = new RegExp('.*' + episode.getEpisodeNumber() + '.*', 'ig');
+                //console.log(p.basename(file), reg.test(p.basename(file)), reg);
+                return reg.test(p.basename(file));
+            });
+
+            if (episodeFile) {
+                fs.outputFileSync(basefolder + episode.getFileName(), episode.toXML(), 'utf8');
+
+                var newEpisodeFile = basefolder + episode.getFileName(p.extname(episodeFile));
+
+                //move episode files
+                if (!fs.existsSync(newEpisodeFile)) {
+                    fs.move(episodeFile, newEpisodeFile, {}, function(err) {
+                        if (err) {
+                            console.error(err);
+                        }
+                        callback();
+                    });
+                }
+                else {
+                    callback();
+                }
+            }
+            else {
+                callback();
+            }
+            //});
+        }, function(err) {
+            if (err) {
+                console.error(err);
+            }
+            console.log(sprintf('   All episodes are processed.'));
+            fulfill();
+        });
+    });
+}
+
+function getPoster(show, basefolder) {
+    // get poster image, crop it and multiply
+    return util.downloadFile(show.images.packageLarge, basefolder + show.getPosterFileName())
+        .then(function() {
+            return util.cropPoster(basefolder + show.getPosterFileName());
+        })
+        .then(function() {
+
+            fs.copySync(basefolder + show.getPosterFileName(), basefolder + show.getPosterFileName('season01-'));
+            fs.copySync(basefolder + show.getPosterFileName(), basefolder + show.getPosterFileName('season-all-'));
+            //console.log('poster done');
+        });
+}
+
+function getFanart(show, basefolder) {
+    // get fanart image and multiply
+    if (show.images.baseImages.length > 0) {
+        return util.downloadFile(show.images.baseImages[0], basefolder + show.getFanartFileName())
+            .then(function() {
+                fs.copySync(basefolder + show.getFanartFileName(), basefolder + show.getFanartFileName('season01-'));
+                fs.copySync(basefolder + show.getFanartFileName(), basefolder + show.getFanartFileName('season-all-'));
+
+                //console.log('fanart done');
+            });
+    }
+}
+
+function moveShow(src, dest) {
+    // moving base folder
+    if (!fs.existsSync(dest)) {
+
+        return new Promise(function(fulfill, reject) {
+            fs.move(src, dest, function() {
+                //console.log('move done');
+                fulfill();
+            });
+        });
+    }
+}
