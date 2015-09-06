ba2dcba347b4d4554c3345d018b56401fa7f6bf1
AleksueiR <aleksuei.riabtsev@ec.gc.ca>
2015-09-06 17:14:28 -0400

    fix(writer): use rename instead of move
    Rename is faster and moving across partitions is unlikely.

--- src/writer.js
+++ src/writer.js
@@ -39,9 +39,9 @@ module.exports.writeShowInfo = function(record) {
             return processEpisodes(show, episodeFiles, basefolder)
                 .then(function() {
                     console.log(sprintf('   Fetching images...'));
-                    
-                    //console.log('poster');        
-                    
+
+                    //console.log('poster');
+
                     return getPoster(show, basefolder);
                 })
                 .then(function() {
@@ -52,7 +52,7 @@ module.exports.writeShowInfo = function(record) {
                     //console.log('move');
                     return moveShow(record.dir, p.dirname(record.dir) + p.sep + show.getShowTitle());
                 });
-            
+
         })
         .then(function() {
             console.log('All done.');
@@ -62,9 +62,9 @@ module.exports.writeShowInfo = function(record) {
 function processEpisodes(show, episodeFiles, basefolder) {
     return new Promise(function(fulfill, reject) {
         async.eachSeries(show.episodes, function(episode, callback) {
-            
+
             //console.log(episode);
-            
+
             //show.episodes.forEach(function(episode) {
             var episodeFile = find(episodeFiles, function(file) {
                 var reg = new RegExp('.*' + episode.getEpisodeNumber() + '.*', 'ig');
@@ -79,7 +79,8 @@ function processEpisodes(show, episodeFiles, basefolder) {
 
                 //move episode files
                 if (!fs.existsSync(newEpisodeFile)) {
-                    fs.move(episodeFile, newEpisodeFile, {}, function(err) {
+                    //fs.move(episodeFile, newEpisodeFile, {}, function(err) {
+                    fs.rename(episodeFile, newEpisodeFile, function(err) {
                         if (err) {
                             console.error(err);
                         }
@@ -133,13 +134,19 @@ function getFanart(show, basefolder) {
 
 function moveShow(src, dest) {
     // moving base folder
+    //console.log('moving')
     if (!fs.existsSync(dest)) {
 
         return new Promise(function(fulfill, reject) {
-            fs.move(src, dest, function() {
+            fs.rename(src, dest, function() {
                 //console.log('move done');
                 fulfill();
             });
+
+            /*fs.move(src, dest, function() {
+                console.log('move done');
+                fulfill();
+            });*/
         });
     }
 }
